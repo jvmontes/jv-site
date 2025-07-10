f987fc2f6d285abf05edfd0700cb90a55bd93a08
Jorge Viramontes <>
2025-07-10 17:45:00 -0500

    fix some issues with the image stretching the whole hero

--- src/components/headerNav.js
+++ src/components/headerNav.js
@@ -1,10 +1,9 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import * as styles from "./headerNav.module.css"
-
 import { IconContext } from "react-icons"
-
 import { FiBook, FiUser, FiMail, FiMusic } from "react-icons/fi"
+import { useLocation } from "@reach/router"
 
 const activeStyles = {
   textDecoration: `underline`,
@@ -12,60 +11,69 @@ const activeStyles = {
   textAlign: "center",
 }
 
-const HeaderNav = () => (
-  <div className={styles.headerNav}>
-    <Link
-      to="/blog"
-      className={styles.linkContainer}
-      activeStyle={activeStyles}
-    >
-      <IconContext.Provider value={{ color: "#083f45", size: "1.5rem" }}>
-        <span>
-          <FiBook></FiBook>
-        </span>
-      </IconContext.Provider>
-      Blog
-    </Link>
+const HeaderNav = () => {
+  const location = useLocation()
+  const isHome = location.pathname === "/"
+  const iconColor = isHome ? "#fff" : "#083f45"
+  const navClass = isHome
+    ? `${styles.headerNav} ${styles.navWhite}`
+    : styles.headerNav
 
-    <Link
-      to="/music"
-      className={styles.linkContainer}
-      activeStyle={activeStyles}
-    >
-      <IconContext.Provider value={{ color: "#083f45", size: "1.5rem" }}>
-        <span>
-          <FiMusic></FiMusic>
-        </span>
-      </IconContext.Provider>
-      Music
-    </Link>
+  return (
+    <div className={navClass}>
+      <Link
+        to="/blog"
+        className={styles.linkContainer}
+        activeStyle={activeStyles}
+      >
+        <IconContext.Provider value={{ color: iconColor, size: "1.5rem" }}>
+          <span>
+            <FiBook />
+          </span>
+        </IconContext.Provider>
+        Blog
+      </Link>
 
-    <Link
-      to="/about"
-      className={styles.linkContainer}
-      activeStyle={activeStyles}
-    >
-      <IconContext.Provider value={{ color: "#083f45", size: "1.5rem" }}>
-        <span>
-          <FiUser></FiUser>
-        </span>
-      </IconContext.Provider>
-      About
-    </Link>
+      <Link
+        to="/music"
+        className={styles.linkContainer}
+        activeStyle={activeStyles}
+      >
+        <IconContext.Provider value={{ color: iconColor, size: "1.5rem" }}>
+          <span>
+            <FiMusic />
+          </span>
+        </IconContext.Provider>
+        Music
+      </Link>
 
-    <Link
-      to="/contact-me"
-      className={styles.linkContainer}
-      activeStyle={activeStyles}
-    >
-      <IconContext.Provider value={{ color: "#083f45", size: "1.5rem" }}>
-        <span>
-          <FiMail></FiMail>
-        </span>
-      </IconContext.Provider>
-      Contact
-    </Link>
-  </div>
-)
+      <Link
+        to="/about"
+        className={styles.linkContainer}
+        activeStyle={activeStyles}
+      >
+        <IconContext.Provider value={{ color: iconColor, size: "1.5rem" }}>
+          <span>
+            <FiUser />
+          </span>
+        </IconContext.Provider>
+        About
+      </Link>
+
+      <Link
+        to="/contact-me"
+        className={styles.linkContainer}
+        activeStyle={activeStyles}
+      >
+        <IconContext.Provider value={{ color: iconColor, size: "1.5rem" }}>
+          <span>
+            <FiMail />
+          </span>
+        </IconContext.Provider>
+        Contact
+      </Link>
+    </div>
+  )
+}
 
 export default HeaderNav
