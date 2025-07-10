d13156d512874b8466b26c7fcdb0f64705e6ef50
Jorge Viramontes <>
2025-07-10 17:27:25 -0500

    add background image to home page.

--- src/components/header.js
+++ src/components/header.js
@@ -3,21 +3,36 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import * as headerStyles from "./header.module.css"
 import { StaticImage } from "gatsby-plugin-image"
-
-
 import HeaderNav from "./headerNav"
+import { useLocation } from "@reach/router"
 
-const Header = ({ siteTitle }) => (
-  <header className={headerStyles.header}>
-    <Link to="/">
-      <StaticImage
-        src="../images/jv.png" width={80} quality={95}
-        formats={["AUTO", "WEBP", "AVIF"]} alt="JV Logo"
-      />
-    </Link>
-    <HeaderNav></HeaderNav>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const location = useLocation()
+  const isHome = location.pathname === "/"
+
+  return (
+    <header
+      className={
+        isHome
+          ? `${headerStyles.header} ${headerStyles.headerTransparent}`
+          : headerStyles.header
+      }
+    >
+      <div className={headerStyles.headerContent}>
+        <Link to="/">
+          <StaticImage
+            src="../images/jv.png"
+            width={80}
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="JV Logo"
+          />
+        </Link>
+        <HeaderNav></HeaderNav>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
