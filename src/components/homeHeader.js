aff588029223380f5e8422e2e22c7ac4c7a92a75
Jorge Viramontes <>
2025-07-17 16:03:48 -0500

    change copy on home page and fit to 100vh

--- src/components/homeHeader.js
+++ src/components/homeHeader.js
@@ -11,22 +11,13 @@ const homeHeader = () => {
         <div className={styles.bannerDetails}>
           <div className={styles.title}>Jorge Viramontes</div>
 
-          <h4 className={styles.tagline}>Entrepreneur, Engineer, and Empath</h4>
+          <h4 className={styles.tagline}>Engineer, Educator, and Speaker</h4>
 
           <div>
-            <p>
-              Jorge brings over seven years of Fortune 500 consulting experience
-              to the digital entrepreneurship realm from Chicago, IL.
-            </p>
+            <p>Helping people make sense of tech, business, and AI.</p>
 
             <p>
-              He's the owner of Alacrán Studio, a company focused on growing
-              small businesses by building simple, powerful websites.
-            </p>
-
-            <p>
-              When he's not coding or building his business, he loves traveling,
-              meeting new people, and listening to all kinds of music.
+              Creative expression and storytelling through music and writing.
             </p>
 
             <CommonButton
